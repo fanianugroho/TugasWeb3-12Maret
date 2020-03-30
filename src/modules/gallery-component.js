27eaaccce1934e7ef51db69d71b999b261318abb
fanianugroho <hanifanianugroho@gmail.com>
2020-03-30 12:44:42 +0700

    ll

--- src/modules/gallery-component.js
+++ src/modules/gallery-component.js
@@ -37,7 +37,7 @@ class PortofolioComponent extends Component{
                                 <br/>
                                 <br/>
                                 <br/>
-                            <div className="site-card-wrapper-transparant" style={{ marginRight: 80 }}>
+                            <div className="site-card-wrapper-transparant" style={{ marginRight: 190 }}>
                             <Row gutter={100}>
                                 <Col span={90}>
                                 <Card
